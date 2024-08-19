84ac34d510d88668b1ea822b2d7b6e923994f795
Emanuel <emanuel.snasc1@gmail.com>
2024-08-19 00:16:26 -0300

    youtube live and twitch chat implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,108 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [streamUser, setStreamUser] = React.useState("");
+  const [youtubeURL, setYoutubeURL] = React.useState("");
+  const [show, setShow] = React.useState(false);
+
+  const colors = [
+    "#FF8A80", "#FF80AB", "#EA80FC",
+    "#F5F5F5", "#66BB6A", "#B388FF",
+    "#1976D2", "#0097A7", "#F57C00",
+  ];
+
+  const currentMessages = React.useRef(["Primeira mensagem"]);
+  const socket = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
+
+  const randomIntFromInterval = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const onSubmit = (eventSubmit: React.FormEvent<HTMLFormElement>) => {
+    eventSubmit.preventDefault();
+    setShow(true);
+    socket.onmessage = (event) => {
+      const message = event.data.trim();
+
+      if (message.startsWith('PING')) {
+        socket.send('PONG :tmi.twitch.tv'); // Responder ao PING da Twitch
+      }
+
+      if (message.includes('PRIVMSG')) {
+        const user = message.split('!')[0].substring(1);
+        const chatMessage = message.split('PRIVMSG')[1].split(':')[1];
+
+        const randomNumber = randomIntFromInterval(1, 9);
+
+        const ulElement = document.getElementById("chat");
+        if (ulElement) {
+          const liElement = document.createElement("li");
+          liElement.className = "chatItem";
+          liElement.style.setProperty("color", colors[randomNumber]);
+          liElement.innerHTML = `<p class="chatText">${user}: ${chatMessage}</p>`
+          ulElement.appendChild(liElement);
+          currentMessages.current.push(`${user}: ${chatMessage}`);
+          if (currentMessages.current.length > 44) {
+            currentMessages.current.shift();
+            ulElement.querySelector('li')!.remove()
+          }
+          setTimeout(() => { ulElement.scrollTop = ulElement.scrollHeight; }, 0);
+        }
+      }
+    };
+
+    // Autenticar (você pode usar 'justinfan12345' como nome de usuário para acesso anônimo)
+    socket.onopen = function () {
+      const oauthToken = `oauth:${import.meta.env.VITE_ACCESS_KEY}`;
+
+      socket.send(`PASS ${oauthToken}`);
+      socket.send(`NICK ${import.meta.env.VITE_USERNAME}`);
+      socket.send(`JOIN #${streamUser}`); // Nome do canal que você quer entrar
+    };
+
+    socket.onerror = function (error) {
+      console.error('Erro no WebSocket:', error);
+    };
+
+    socket.onclose = function () {
+      console.log('Conexão WebSocket fechada.');
+    };
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="content">
+      {(streamUser && youtubeURL && show) ? (
+        <iframe
+          src={youtubeURL} title="YouTube video player" frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>
+        </iframe>
+      ) : (
+        <div className="dataForm">
+          <form onSubmit={onSubmit}>
+            <div className="textInput">
+              <label>Stream User</label>
+              <input
+                onChange={(event) => setStreamUser(event.target.value)}
+              />
+            </div>
+            <div className="textInput">
+              <label>Youtube URL</label>
+              <input
+                onChange={(event) => setYoutubeURL(event.target.value)}
+              />
+            </div>
+            <button>Enviar</button>
+          </form>
+        </div>
+      )}
+      <section className="chatSection">
+        <h1 className="title">Chat</h1>
+        <ul className="chatBox" id="chat"></ul>
+      </section>
+    </div>
   )
 }
 
